64043abc5f7949a54e7f7b78469340c893ff833a
BilelMatallah <107211669+BilelMatallah@users.noreply.github.com>
2023-04-18 12:56:21 +0000

    feat: first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,69 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+const HEADERS = { "Content-Type": "application/json" }
+const URL = "https://assets.breatheco.de/apis/fake/todos/user/bill"
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [state, setState] = useState([]);
+	const [load, setLoad] = useState(false);
+
+	
+
+	const getData = async () => {
+		try {
+			setLoad(true)
+			const response = await fetch (URL, {method: "GET"});
+			const data = await response.json();
+			setState(data);
+			setLoad(false);
+		}catch(err){
+			console.log("error", err)
+		}
+
+	}
+
+	useEffect(() => {
+		getData()
+	},[]);
+
+	const addNewTodo = async () => {
+		try {
+			setLoad(true)
+			const body = [...state, { label: "newTodo", done: false }]
+			const response = await fetch(URL, { method: "PUT", body: JSON.stringify(body), headers: HEADERS });
+			console.log(response);
+			await getData();
+			setLoad(false);
+		}catch(err){
+			console.log("error", err)
+		}
+	}
+
+	const deleteTodo = async (indice) => {
+		try {
+
+			const newState = state.filter ((elem, index) => {return indice !== index})
+
+			setLoad(true)
+			const response = await fetch(URL, { method: "PUT", body: JSON.stringify(newState), headers: HEADERS });
+			console.log(response);
+			getData();
+			setLoad(false);
+		}catch(err){
+			console.log("error", err)
+		}
+	}
+
+
+
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			{load ? <div>Loading...</div> : state.map((todo)=> <div key={todo.label}> {todo.label} <button onClick={deleteTodo}>delete</button></div> )}
+			<button onClick={addNewTodo}>Add task</button>
+			
 		</div>
 	);
 };
