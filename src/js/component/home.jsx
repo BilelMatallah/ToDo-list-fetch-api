e6450dd73089eed5a5245f6e5ec2229fb4f02dcc
BilelMatallah <bilelmatallah@hotmail.com>
2023-06-11 20:30:05 +0000

    feat: tarea terminada

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import AddTask from "./AddTask.jsx";
 const HEADERS = { "Content-Type": "application/json" }
 const URL = "https://assets.breatheco.de/apis/fake/todos/user/bill"
 
@@ -30,25 +31,26 @@ const Home = () => {
 	const addNewTask = async () => {
 		try {
 			setLoad(true)
-			const body = [...list, { label: newTask, done: false }]
-			const response = await fetch(URL, { method: "PUT", body: JSON.stringify(body), headers: HEADERS });
+			const data = [...list, { label: newTask, done: false }]
+			const response = await fetch(URL, { method: "PUT", body: JSON.stringify(data), headers: HEADERS });
 			console.log(response);
-			await getData();
+			getData();
 			setLoad(false);
-		}catch(err){
-			console.log("error", err)
+		}catch(error){
+			console.log("error", error)
 		}
 	}
 
-// "sacar el perro" = "tarea" 
+
 
 	const deleteTodo = async (tarea) => {
 		try {
 			const newState = list.filter ((elem) => elem.label != tarea)
 			setLoad(true)
-			const response = await fetch(URL, { method: "PUT", body: JSON.stringify(newState), headers: HEADERS });
-			console.log(response);
-			getData();
+			const response = await fetch(URL, { method: "PUT", body : JSON.stringify(newState), headers: HEADERS });
+			console.log(response, 'hhhhh');
+			await getData();
+			setList(newState)
 			setLoad(false);
 		}catch(err){
 			console.log("error", err)
@@ -57,9 +59,13 @@ const Home = () => {
 
 	return (
 		<div className="text-center">
-			{load ? <div>Loading...</div> : list.map((task)=> <div key={task.label}> {task.label} <button onClick={ () => {deleteTodo(task.label)} }>delete</button></div> )}
-			<input className="input" onChange={(e) => setNewTask(e.target.value)} type="text" placeholder="write down your task" />
-			<button onClick={addNewTask}>Add task</button>
+			<div className="input-div">
+				<input className="input" onChange={(e) => setNewTask(e.target.value)} type="text" placeholder="write down your task" />
+				<AddTask addNewTask={addNewTask}/>
+				{load ? <div className="spinner spinner-border m-5" role="status">
+				</div>
+				: list.map((task)=> <div className="card mt-3" key={task.label}> {task.label} <button className="btn btn-danger mt-1" onClick={ () => {deleteTodo(task.label)} }>delete</button></div> )}
+			</div>
 		</div>
 	);
 };
